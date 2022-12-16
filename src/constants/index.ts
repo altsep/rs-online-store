47cac6975f8ba315beebac84eda5489ad7f60465
Alexei Arushanov <mx24xnpi4@mozmail.com>
2022-12-16 15:55:42 +0300

    refactor: remove unused global property isDefault
    disable eslint for data - it is assumed to have several exports

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,7 +11,6 @@ interface State {
 
 interface Props {
   state: State;
-  isDefault?: boolean;
   parentNodeName: string;
   path: string;
 }
@@ -22,7 +21,6 @@ const defaults: Props = {
   state: {
     products: tempProductsData, // When creating a new object from default properties, arrays are passed by link and have to be copied separately. This can cause unexpected results when modifying a "copy" while trying to use the initial array which ends up being modified
   }, // Mutable properties go in state, it being the collection of initial values
-  isDefault: false,
   parentNodeName: 'main',
   path: '/products',
 };
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,4 @@
 import tempProductsData from './tempProductsData';
 
-export { tempProductsData };
+// eslint-disable-next-line import/prefer-default-export
+export { tempProductsData }; // Assumed to have multiple exports
