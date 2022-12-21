c89efae6e36dc3e91b668b0e5d17d1da140025ab
Alexei Arushanov <mx24xnpi4@mozmail.com>
2022-12-21 08:37:00 +0300

    refactor: remove redundant code and make conditional less obscure in updateCartCount

--- src/components/header/updateCartCount.ts
+++ src/components/header/updateCartCount.ts
@@ -1,18 +1,19 @@
 import { State } from '../../constants';
 
 export default function updateCartCount(state: State): void {
-  const s = state;
-  const { totalSum, itemsInCart } = s;
+  const { totalSum, itemsInCart } = state;
 
   const cartCount = document.querySelector<HTMLDivElement>('.cart-count');
   const cartTotalSum = document.querySelector<HTMLSpanElement>('.cart-total');
 
   if (cartCount && cartTotalSum) {
     cartCount.textContent = '';
-    if (itemsInCart) {
+
+    if (itemsInCart > 0) {
       cartCount.style.backgroundColor = 'rgb(192, 31, 58)';
       cartCount.textContent = `${itemsInCart}`;
     }
+
     cartTotalSum.textContent = `${totalSum}`;
   }
 }
