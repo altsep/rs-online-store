d3bfb92adaaa2c869793e58a465b43057c5f338e
Alexei Arushanov <mx24xnpi4@mozmail.com>
2022-12-22 18:26:45 +0300

    fix: typo in cartTotalTitle, use regular string for its textContent

--- src/components/header/headerCartTotal.ts
+++ src/components/header/headerCartTotal.ts
@@ -5,9 +5,9 @@ export default function initHeaderCartTotal(parentNodeName: string): void {
     const cartTotalContainer = parentNode.appendChild(document.createElement('div'));
     cartTotalContainer.className = 'cart-total-container';
 
-    const cartTolalTitle = cartTotalContainer.appendChild(document.createElement('span'));
-    cartTolalTitle.className = 'cart-total-text';
-    cartTolalTitle.textContent = `Cart total: `;
+    const cartTotalTitle = cartTotalContainer.appendChild(document.createElement('span'));
+    cartTotalTitle.className = 'cart-total-text';
+    cartTotalTitle.textContent = 'Cart total: ';
 
     const cartTotalSum = cartTotalContainer.appendChild(document.createElement('span'));
     cartTotalSum.className = 'cart-total';
