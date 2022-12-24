88214ab0d2d3455c7083488244fdf536ea35b844
Alexei Arushanov <mx24xnpi4@mozmail.com>
2022-12-24 09:23:41 +0300

    refactor: pass parent component to header children instead of its name
    remove unnecessary code

--- src/components/header/headerCart.ts
+++ src/components/header/headerCart.ts
@@ -1,23 +1,19 @@
 import { handleHistory } from '../../utility';
 import cartLogoImg from './img/cart.svg';
 
-export default function initHeaderCart(parentNodeName: string): void {
-  const parentNode = document.querySelector(parentNodeName || '');
+export default function initHeaderCart(parent: HTMLElement): void {
+  const cartContainer = parent.appendChild(document.createElement('div'));
+  cartContainer.className = 'cart-container';
 
-  if (parentNode) {
-    const cartContainer = parentNode.appendChild(document.createElement('div'));
-    cartContainer.className = 'cart-container';
+  const cartLogo = cartContainer.appendChild(document.createElement('img'));
+  cartLogo.className = 'cart-logo';
+  cartLogo.src = cartLogoImg;
+  cartLogo.alt = '';
 
-    const cartLogo = cartContainer.appendChild(document.createElement('img'));
-    cartLogo.className = 'cart-logo';
-    cartLogo.src = cartLogoImg;
-    cartLogo.alt = '';
+  const cartCount = cartContainer.appendChild(document.createElement('div'));
+  cartCount.className = 'cart-count';
 
-    const cartCount = cartContainer.appendChild(document.createElement('div'));
-    cartCount.className = 'cart-count';
-
-    cartContainer.addEventListener('click', () => {
-      handleHistory('/cart');
-    });
-  }
+  cartContainer.addEventListener('click', () => {
+    handleHistory('/cart');
+  });
 }
