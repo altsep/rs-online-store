2ace45c70f07298d2396b5139acc9db5b6a6bebd
AnnaFeona <103384130+AnnaFeona@users.noreply.github.com>
2023-01-01 22:02:44 +0300

    style: add not-found page styles

--- src/components/notFound/index.ts
+++ src/components/notFound/index.ts
@@ -3,31 +3,31 @@ import errorImage from '../../assets/images/404.png';
 
 function renderNotFound(): void {
   const notFoundNode = document.createElement('div');
-  notFoundNode.className = 'not-found';
+  notFoundNode.className = 'wrapper not-found' ;
 
   const notFoundContainer = document.createElement('div');
   notFoundContainer.className = 'not-found__container';
 
-  const errNumber = document.createElement('div');
-  errNumber.className = 'err-number';
-  errNumber.textContent = '404';
+  const errTitle = document.createElement('h2');
+  errTitle.className = 'err-title';
+  errTitle.textContent = 'Not found';
 
-  const errText = document.createElement('h2');
-  errText.className = 'err-text';
-  errText.textContent = 'The page cannot be found';
+  const errMessage = document.createElement('div');
+  errMessage.className = 'err-message';
+  errMessage.textContent = 'The page you are looking for is unavailable';
 
-  const button = document.createElement('button');
-  button.className = 'btn';
-  button.textContent = '--> To main page';
+  const goHomebtn = document.createElement('button');
+  goHomebtn.className = 'btn go-home__btn';
+  goHomebtn.textContent = 'Click here to go back to the HomePage';
 
-  notFoundContainer.append(errNumber, errText, button);
+  notFoundContainer.append(errTitle, errMessage,  goHomebtn);
 
   const notFoundImage = document.createElement('img');
   notFoundImage.className = 'not-found__background';
   notFoundImage.src = errorImage;
   notFoundImage.alt = 'Not Found';
 
-  button.onclick = () => {
+  goHomebtn.onclick = () => {
     handleHistory('/');
   }
 
