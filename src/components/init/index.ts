b003e0af9638dfe6b557781e951c5c5f3490f874
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-01-09 23:11:52 +0300

    docs: print self-assesment scores to console

--- src/components/init/index.ts
+++ src/components/init/index.ts
@@ -30,6 +30,13 @@ function init(): void {
   onNavigate();
 
   window.addEventListener('popstate', onNavigate);
+
+  console.log('store-page: 120');
+  console.log('cart-page: 60');
+  console.log('purchase-modal: 50');
+  console.log('product-details-page: 40');
+  console.log('header-module: 20');
+  console.log('404-page: 10');
 }
 
 export { init };
